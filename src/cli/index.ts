2dc3a93156317053d76d351acd2a6aba60ad8f17
Cruise Dayne <atif@fivexlabs.com>
2025-06-18 17:57:35 +0500

    Updated version number of npm package and CLI

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -272,7 +272,7 @@ function resolveCssPath(config: any, workspace: any): string {
 program
   .name('fivui')
   .description('FivUI - Modern UI Component Library CLI')
-  .version('0.9.2');
+  .version('0.9.3');
 
 program
   .command('init')
@@ -325,7 +325,7 @@ program.action(() => {
   const isMonorepo = workspace.type !== 'single';
   
   console.log('\n🎨 FivUI - Modern UI Component Library');
-  console.log('Version: 0.9.2\n');
+  console.log('Version: 0.9.3\n');
   
   if (isMonorepo) {
     console.log(`🏢 Detected ${workspace.type} monorepo`);

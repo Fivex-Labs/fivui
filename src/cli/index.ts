e0713dd2278388eee612a14d389f942f0435aff1
Cruise Dayne <atif@fivexlabs.com>
2025-06-19 18:16:58 +0500

    Added a new component called Pagination

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -272,7 +272,7 @@ function resolveCssPath(config: any, workspace: any): string {
 program
   .name('fivui')
   .description('FivUI - Modern UI Component Library CLI')
-  .version('0.13.3');
+  .version('0.13.4');
 
 program
   .command('init')
@@ -325,7 +325,7 @@ program.action(() => {
   const isMonorepo = workspace.type !== 'single';
   
   console.log('\n🎨 FivUI - Modern UI Component Library');
-  console.log('Version: 0.13.3\n');
+  console.log('Version: 0.13.4\n');
   
   if (isMonorepo) {
     console.log(`🏢 Detected ${workspace.type} monorepo`);

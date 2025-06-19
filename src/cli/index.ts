88667155a0f9691b488dfaa51fab04dabff17765
Cruise Dayne <atif@fivexlabs.com>
2025-06-19 23:51:11 +0500

    Made changes to make data table more dynamic

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -272,7 +272,7 @@ function resolveCssPath(config: any, workspace: any): string {
 program
   .name('fivui')
   .description('FivUI - Modern UI Component Library CLI')
-  .version('0.13.6');
+  .version('0.13.7');
 
 program
   .command('init')
@@ -355,7 +355,7 @@ program.action(() => {
   const isMonorepo = workspace.type !== 'single';
   
   console.log('\n🎨 FivUI - Modern UI Component Library');
-  console.log('Version: 0.13.6\n');
+  console.log('Version: 0.13.7\n');
   
   if (isMonorepo) {
     console.log(`🏢 Detected ${workspace.type} monorepo`);

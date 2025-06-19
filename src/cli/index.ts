38150dd4947c62cdc8f2c9f825c3446407ceaa3a
Cruise Dayne <atif@fivexlabs.com>
2025-06-19 17:20:54 +0500

    Fixed data format to get rid of hydration issues

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -272,7 +272,7 @@ function resolveCssPath(config: any, workspace: any): string {
 program
   .name('fivui')
   .description('FivUI - Modern UI Component Library CLI')
-  .version('0.13.1');
+  .version('0.13.2');
 
 program
   .command('init')
@@ -325,7 +325,7 @@ program.action(() => {
   const isMonorepo = workspace.type !== 'single';
   
   console.log('\n🎨 FivUI - Modern UI Component Library');
-  console.log('Version: 0.12.1\n');
+  console.log('Version: 0.13.2\n');
   
   if (isMonorepo) {
     console.log(`🏢 Detected ${workspace.type} monorepo`);
